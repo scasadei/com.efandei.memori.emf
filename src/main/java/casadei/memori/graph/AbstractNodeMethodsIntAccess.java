2f34804c927cb301eab2f623df1d127c31c1d204
stefano casadei <scasadei@yahoo.com>
2010-11-20 00:35:50 +0100

    topic branch: trying to using exceptions for non implemented methods

--- src/main/java/casadei/memori/graph/AbstractNodeMethodsIntAccess.java
+++ src/main/java/casadei/memori/graph/AbstractNodeMethodsIntAccess.java
@@ -2,7 +2,10 @@ package casadei.memori.graph;
 
 public class AbstractNodeMethodsIntAccess {
 
-	public boolean isNode(int ni) { return false;}
+	public boolean isNode(int ni) throws MethodNotImplemented { 
+		throw new AbstractNodeMethodsIntAccess.MethodNotImplemented();
+	}
+
 	public boolean isLeaf (int ni) { return false;}
 	public int getPhylum(int ni) { return -1;}
 	public int getGenus(int ni)  { return -1;}
@@ -10,4 +13,13 @@ public class AbstractNodeMethodsIntAccess {
 	public void print(int ni)  {}
 	public String toString(int ni) { return "To Be Done"; }
 
+	public void treatNonImplementedMethod() throws MethodNotImplemented {
+		throw new AbstractNodeMethodsIntAccess.MethodNotImplemented();
+	}
+	
+	class MethodNotImplemented extends Exception {
+		private static final long serialVersionUID = 6964209639344664249L;
+		MethodNotImplemented() { super(); }
+		MethodNotImplemented(final String s) { super(s); }
+	}
 }
